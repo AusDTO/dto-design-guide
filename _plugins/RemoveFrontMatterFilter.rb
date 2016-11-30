5d5c074c5c767f350bbbd94dec24d7510623c917
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2016-11-30 13:26:57 +1100

    incomplete search efforts
    Done:
    - created a js object for the entire site ready for searching
    
    Missing:
    - js for it to return the search object
    - precompile the js via node
    - search html and result html
    
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

---
+++ _plugins/RemoveFrontMatterFilter.rb
@@ -0,0 +1,25 @@
+module Jekyll
+  module RemoveFrontMatterFilter
+    def removefrontmatter input
+      # find the first
+      first = input.index("---")
+      if (first == nil)
+        input
+      end
+
+      # find the second
+      second = input.index("---", first + 1)
+      if (second == nil)
+        input
+      end
+
+      #{}"First: #{first} Second: #{second}"
+
+      #strip the string
+      input[first..second + 2] = ''
+      #return input
+      input
+    end
+  end
+end
+Liquid::Template.register_filter Jekyll::RemoveFrontMatterFilter
